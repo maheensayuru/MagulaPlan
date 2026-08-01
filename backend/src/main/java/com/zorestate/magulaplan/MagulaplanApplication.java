c82c594812a50e9b81e27b40fbe33da1d839a4c9
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-01 23:54:49 +0530

    chore: Add initial Spring Boot project structure

---
+++ backend/src/main/java/com/zorestate/magulaplan/MagulaplanApplication.java
@@ -0,0 +1,13 @@
+package com.zorestate.magulaplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagulaplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagulaplanApplication.class, args);
+	}
+
+}
