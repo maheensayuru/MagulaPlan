b40f999bb29cce76031a8fdffd90dbc043597063
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-02 23:53:47 +0530

    fix: Correct package name from 'com.zorestate' to 'com.zerostate' in multiple files

--- backend/src/main/java/com/zerostate/magulaplan/MagulaplanApplication.java
+++ backend/src/main/java/com/zerostate/magulaplan/MagulaplanApplication.java
@@ -1,4 +1,4 @@
-package com.zorestate.magulaplan;
+package com.zerostate.magulaplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
