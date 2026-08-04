66faa4e0feaa5bdf369b951aa6d07f0d324dd998
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-04 19:25:55 +0530

    feat: Update GuestService interface

--- backend/src/main/java/com/zerostate/magulaplan/service/GuestService.java
+++ backend/src/main/java/com/zerostate/magulaplan/service/GuestService.java
@@ -1,10 +1,16 @@
 package com.zerostate.magulaplan.service;
 
+import com.zerostate.magulaplan.dto.GuestRequestDto;
+import com.zerostate.magulaplan.dto.GuestResponseDto;
 import com.zerostate.magulaplan.entity.Guest;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface GuestService {
-
+    GuestResponseDto saveGuest(GuestRequestDto guestRequestDto);
+    List<GuestResponseDto> getAllGuests();
+    GuestResponseDto getGuestById(UUID guestId);
+    GuestResponseDto updateGuest(UUID guestId, GuestRequestDto guestRequestDto);
+    Void deleteGuest(UUID guestId);
 }
