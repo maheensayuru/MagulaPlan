ab377fb2c92cc84dff0da08b9746d14eba79edff
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-05 07:13:09 +0530

    feat: Implement GuestService with basic CRUD methods and repository integration

--- backend/src/main/java/com/zerostate/magulaplan/service/impl/GuestServiceImpl.java
+++ backend/src/main/java/com/zerostate/magulaplan/service/impl/GuestServiceImpl.java
@@ -1,4 +1,43 @@
 package com.zerostate.magulaplan.service.impl;
 
-public class GuestServiceImpl {
+import com.zerostate.magulaplan.dto.GuestRequestDto;
+import com.zerostate.magulaplan.dto.GuestResponseDto;
+import com.zerostate.magulaplan.repo.GuestRepository;
+import com.zerostate.magulaplan.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class GuestServiceImpl implements GuestService {
+
+    @Autowired
+    private GuestRepository guestRepository;
+
+    @Override
+    public GuestResponseDto saveGuest(GuestRequestDto guestRequestDto) {
+        return null;
+    }
+
+    @Override
+    public List<GuestResponseDto> getAllGuests() {
+        return List.of();
+    }
+
+    @Override
+    public GuestResponseDto getGuestById(UUID guestId) {
+        return null;
+    }
+
+    @Override
+    public GuestResponseDto updateGuest(UUID guestId, GuestRequestDto guestRequestDto) {
+        return null;
+    }
+
+    @Override
+    public Void deleteGuest(UUID guestId) {
+        return null;
+    }
 }
