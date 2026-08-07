28f23e5008ad34d4a1742299d198df32958e4c5b
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-07 21:03:09 +0530

    feat: Implement UserService methods for user management

--- backend/src/main/java/com/zerostate/magulaplan/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/zerostate/magulaplan/service/impl/UserServiceImpl.java
@@ -1,4 +1,42 @@
 package com.zerostate.magulaplan.service.impl;
 
-public class UserServiceImpl {
+import com.zerostate.magulaplan.dto.UserRequestDto;
+import com.zerostate.magulaplan.dto.UserResponseDto;
+import com.zerostate.magulaplan.repo.UserRepository;
+import com.zerostate.magulaplan.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserResponseDto saveUser(UserRequestDto userRequestDto) {
+        return null;
+    }
+
+    @Override
+    public List<UserResponseDto> getAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public UserResponseDto getUserById(Long userId) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto updateUser(Long userId, UserRequestDto userRequestDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+
+    }
 }
