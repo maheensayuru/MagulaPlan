21ef011ff8d6b3d2bb3df808bdde4f2eb6a6c156
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-08 11:04:54 +0530

    feat: Implement saveUser method in UserServiceImpl for user creation

--- backend/src/main/java/com/zerostate/magulaplan/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/zerostate/magulaplan/service/impl/UserServiceImpl.java
@@ -2,10 +2,14 @@ package com.zerostate.magulaplan.service.impl;
 
 import com.zerostate.magulaplan.dto.UserRequestDto;
 import com.zerostate.magulaplan.dto.UserResponseDto;
+import com.zerostate.magulaplan.entity.User;
 import com.zerostate.magulaplan.repo.UserRepository;
 import com.zerostate.magulaplan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -13,13 +17,23 @@ public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository){
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public UserResponseDto saveUser(UserRequestDto userRequestDto) {
-        return null;
+
+        User user = User.builder().fullName(userRequestDto.getFullName()).partnerName(userRequestDto.getPartnerName())
+                .email(userRequestDto.getEmail()).passwordHash(userRequestDto.getPassword())
+                .phoneNumber(userRequestDto.getPhoneNumber())
+                .role(userRequestDto.getRole() != null ? userRequestDto.getRole() : "USER").isActive(true)
+                .weddingDate(userRequestDto.getWeddingDate()).budget(userRequestDto.getTotalBudget())
+                .createdAt(LocalDateTime.from(LocalTime.now())).build();
+        User savedUser = userRepository.save(user);
+
+        return mapToResponseDto(savedUser);
     }
 
     @Override
@@ -40,5 +54,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUser(Long userId) {
 
+    }
+
+    private UserResponseDto mapToResponseDto(User user){
+        return new UserResponseDto(user.getUserId(),user.getFullName(),user.getPartnerName(),user.getEmail(),user.getPhoneNumber(),user.getRole(),user.getIsActive(),user.getWeddingDate(),user.getBudget(),user.getCreatedAt()
+        );
     }
 }
