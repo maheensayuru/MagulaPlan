ee9b6d47f6218591c0269b5321f6e0b1b44e0fcd
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-08 10:45:52 +0530

    feat: Add @Builder annotation to User class

--- backend/src/main/java/com/zerostate/magulaplan/entity/User.java
+++ backend/src/main/java/com/zerostate/magulaplan/entity/User.java
@@ -2,6 +2,7 @@ package com.zerostate.magulaplan.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ import java.util.List;
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
 
     @Id
