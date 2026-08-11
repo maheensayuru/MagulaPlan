6622f0c414d5b10a8c7de3e727a19fbc1b768838
AmandaLakmal <lakmalamanda@gmail.com>
2026-08-11 20:43:40 +0530

    feat: Add ResourceNotFoundException class for handling resource not found errors

---
+++ backend/src/main/java/com/zerostate/magulaplan/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.zerostate.magulaplan.exception;
+
+public class ResourceNotFoundException extends RuntimeException
+{
+    public ResourceNotFoundException(String message)
+    {
+        super(message);
+    }
+}
